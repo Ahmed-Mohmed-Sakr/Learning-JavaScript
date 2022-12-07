329055098ba7e4d15b54a7ad7770d9c6483d928b
Ahmed Sakr <80043395+Ahmed-Mohmed-Sakr@users.noreply.github.com>
2022-12-07 09:39:08 +0200

    promises

---
+++ Functions/promises.js
@@ -0,0 +1,30 @@
+// callback
+const getDataCallback = (callback) => {
+  setTimeout(() => {
+    callback(undefined, "this is the data");
+  }, 2000);
+};
+
+getDataCallback((err, data) => {
+  if (err) {
+  } else {
+    console.log(data);
+  }
+});
+
+// promise
+const myPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    // resolve("this is the promise data");
+    reject("this is my promise error");
+  }, 2000);
+});
+
+myPromise.then(
+  (data) => {
+    console.log(data);
+  },
+  (err) => {
+    console.log(err);
+  }
+);
