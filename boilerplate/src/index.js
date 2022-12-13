af8e30264455269850b2371f18aa41a62b0511db
Ahmed Sakr <80043395+Ahmed-Mohmed-Sakr@users.noreply.github.com>
2022-12-13 11:33:22 +0200

    starting with babel

---
+++ boilerplate/src/index.js
@@ -0,0 +1,13 @@
+//babel input.js -o output.js
+//babel input.js -o output.js --presets env
+const name = "zainb";
+console.log(name);
+
+class Hangman {
+  myMethod() {
+    return "testing";
+  }
+}
+
+const hungman = new Hangman();
+console.log(hungman.myMethod());
